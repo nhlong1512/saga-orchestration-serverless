ed73bc6c41b6d195a35c52c8431d0d90f6bdb4af
nhlong1512 <20521569@m.uit.edu.vn>
2023-12-03 13:27:51 +0700

    chore:CONFIG TERRAFORM - add config terraform

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     # modify the values for the storage account
-    storage_account_name = "sagaorchestratorstorage"
-    container_name       = "testingterraform"
+    storage_account_name = "saga0312"
+    container_name       = "saga03121"
     key                  = "terraform.tfstate"
   }
 }
